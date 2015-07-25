fa655e5e6472460e0d476cd403032b9bfd65b8a0
Bonson Mampilli <bonsonsm@gmail.com>
2015-07-26 05:11:10 +0530

    Completed versions of the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  There are two functions in the class.
+##  1. makeCacheMatrix helps create a matrix object and cache its inverse
+##  2. cacheSolve helps compute the inverse of the matrix
 
-## Write a short comment describing this function
+
+##  This function creates a special "matrix" object 
+##  that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ##  Global variable that would hold the matrix inverse
+    matinverse<<- NULL
+    ##  Setting the matrix
+    set <- function(y) {
+        x <<- y
+        matinverse <<- NULL
+    }
+    ##  Retrieving the matrix
+    get <- function() x
+    ##  Setting the inverse of the Matrix
+    setinverse <- function(x) matinverse <<- solve(x)
+    ##  Getting the inverse of the matrix
+    getinverse <- function() matinverse
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" 
+##  returned by makeCacheMatrix above. 
+##  If the inverse has already been calculated 
+##  (and the matrix has not changed), 
+##  then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    tempm <<- x$getinverse()
+    if(!is.null(tempm)) {
+        ##  Returning the Cached data in the Global Variable
+        print("getting cached data")
+        return(tempm)
+    }
+    ##  Calculating data for the first time
+    data <- x$get()
+    ##  Setting the inverse
+    x$setinverse(data)
+    ##  returning the Global Variable
+    matinverse
 }
